e95d5be2e82e27cd683ed47eaa062c9e48490faf
priya242005 <150768956+priya242005@users.noreply.github.com>
2024-11-13 20:09:32 +0530

    Delete ReactProject directory

--- ReactProject/api.js
+++
@@ -1,8 +0,0 @@
-// api.js
-const API_URL = 'http://localhost:5000/users';
-
-export const fetchData = async () => {
-    const response = await fetch(API_URL);
-    if (!response.ok) throw new Error('Failed to fetch data');
-    return await response.json();
-};
